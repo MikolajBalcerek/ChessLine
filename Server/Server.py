e64d34fefa86655894a1cd68b43db694a432feaf
MikolajBalcerek <mpbalcerek+github@gmail.com>
2017-06-03 22:55:26 +0200

    __matchmaking__ beginnings server side

--- Server/Server.py
+++ Server/Server.py
@@ -6,21 +6,34 @@ class Server(protocol.Protocol):
 
     connectedplayers = 0;
     playingplayers = 0;
+    matchmakingplayers = 0;
 
     def connectionMade(self):
         print ("New connection made at: " + time.strftime("%H:%M"));
+
         Server.connectedplayers = Server.connectedplayers + 1;
+        Server.matchmakingplayers = Server.connectedplayers - Server.playingplayers;
         self.__helloMessages__();
+        self.__matchMaking__();
+
 
     def __helloMessages__(self):
         self.transport.write("Welcome to Chessline. Author: Mikolaj Balcerek, s416040 \n");
         self.transport.write("Server time: " + time.strftime("%H:%M") + "\n");
         self.transport.write("Connected players: " + str(Server.connectedplayers) + "\n");
-        self.transport.write("Available players for matchmaking: " + str(Server.connectedplayers - Server.playingplayers) + "\n");
+        self.transport.write("Available players for matchmaking: " + str(Server.matchmakingplayers) + "\n");
+        self.transport.write("Please wait to be matched with another player" +  "\n");
+
+    def __matchMaking__(self):
+        if (Server.matchmakingplayers >= 2):
+            self.transport.write("Found a pair.. Connecting");
+            Server.playingplayers = Server.playingplayers + 2;
+            Server.matchmakingplayers = Server.matchmakingplayers - 2;
 
     def connectionLost(self, reason):
         Server.connectedplayers = Server.connectedplayers - 1;
         print ("Connection Lost");
+
 
     #def dataReceived(self, data):
     #    "As soon as any data is received, write it back."
